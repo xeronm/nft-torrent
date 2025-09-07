8e18ff099de79a9293e63779bd13741a8de1591b
Denis Muratov <xeronm@gmail.com>
2025-09-07 16:53:46 +0300

    lots of minor fixes

--- NFTorrent/collections.py
+++ NFTorrent/collections.py
@@ -23,7 +23,7 @@ config = CollectionConfig(
             item_cover=cover,
             meta={
                 "name": "Pets Memorial (Test)",
-                "description": "Pets Memorial is an NFT collection that lets you create a lasting digital tribute to your beloved pet. Turn photos and memories into unique NFTs that preserve their story on the blockchain. Share your tribute with others who understand and keep your pet’s memory alive.\nhttps://t.me/pets_memorial_bot",
+                "description": "Pets Memorial is an NFT collection that lets you create a lasting digital tribute to your beloved pet. Turn photos and memories into unique NFTs that preserve their story on the blockchain. Share your tribute with others who understand and keep your pet’s memory alive.",
                 "external_url": "https://testnet.petsmem.site/",
                 "social_links": ["https://t.me/pets_memorial_test_bot", "https://testnet.petsmem.site/"],
                 "cover_image": "https://petsmem.site/images/petsmem-logo-getgems-banner.webp",
